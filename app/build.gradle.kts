3bd5e6ce2c5d83e0e302fe1a59be6ea8c255e596
Askelardd <askelardd1@gmail.com>
2024-01-11 10:39:00 +0000

    TestedeCommit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,12 +46,14 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+
     // dependência do RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // dependências Retrofit
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
@@ -60,4 +62,8 @@ dependencies {
 
     // dependência Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    //CardView
+    implementation ("androidx.cardview:cardview:1.0.0")
+
 }
