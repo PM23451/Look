4a426cdb0111a1b5f70b7cae8b9cb106f9603cd6
PM23451 <120373405+PM23451@users.noreply.github.com>
2024-01-16 14:28:31 +0000

    Commir gradle

--- app/src/main/java/com/example/aprendizagemmobile/dao/AppDatabase.kt
+++ app/src/main/java/com/example/aprendizagemmobile/dao/AppDatabase.kt
@@ -2,9 +2,10 @@ package com.example.aprendizagemmobile.dao
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.aprendizagemmobile.entities.Evento
+import com.example.aprendizagemmobile.entities.Utilizador
 
 
-@Database(entities = [Evento::class], version = 1)
+@Database(entities = [Evento::class, Utilizador::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun eventoDao(): EventoDao //falta adicionar no Main Activity
     abstract fun utilizadorDao(): UtilizadorDao //falta adicionar no Main Activity
--- app/src/main/java/com/example/aprendizagemmobile/templates/UtilizadorViewHolder.kt
+++ app/src/main/java/com/example/aprendizagemmobile/templates/UtilizadorViewHolder.kt
@@ -1,12 +1,13 @@
 package com.example.aprendizagemmobile.templates
 import com.example.aprendizagemmobile.entities.Utilizador
 import androidx.recyclerview.widget.RecyclerView
-import com.example.aprendizagemmobile.databinding.ItemAlunoBinding
+//import com.example.aprendizagemmobile.databinding.ItemAlunoBinding
 class UtilizadorViewHolder {
 
     fun realizarLogin(email: String, password: String) {
-        viewModelScope.launch {
+        /*viewModelScope.launch {
             // Chama o repositório para realizar o login
             _resultadoLogin.value = repository.verificarLogin(email, password)
-        }
+        }*/
+    }
 }
