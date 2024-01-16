703c2df67f8af70c6487f5e3d2f70502e267da4f
PM23451 <120373405+PM23451@users.noreply.github.com>
2024-01-16 15:51:55 +0000

    commit

--- app/src/main/java/com/example/aprendizagemmobile/templates/UtilizadorViewHolder.kt
+++ app/src/main/java/com/example/aprendizagemmobile/templates/UtilizadorViewHolder.kt
@@ -1,13 +1,15 @@
 package com.example.aprendizagemmobile.templates
 import com.example.aprendizagemmobile.entities.Utilizador
 import androidx.recyclerview.widget.RecyclerView
+import com.example.aprendizagemmobile.databinding.ActivityMainBinding
+
 //import com.example.aprendizagemmobile.databinding.ItemAlunoBinding
-class UtilizadorViewHolder(private val binding: ) :
+class UtilizadorViewHolder(private val binding: ActivityMainBinding) :
     RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(utilizador: Utilizador) {
+    /*fun bind(utilizador: Utilizador) {
         binding.Email_Textinput.text = utilizador.email // titulo
         binding.Password_Textinput.text = utilizador.password  //localizacao
 
-    }
+    }*/
 }
