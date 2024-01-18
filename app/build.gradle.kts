a8f9d904901f67af0ff30b06ccb8e7e151227328
PM23451 <120373405+PM23451@users.noreply.github.com>
2024-01-18 21:27:03 +0000

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
 
     // dependência do RecyclerView
     implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("com.google.android.gms:play-services-ads:22.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
